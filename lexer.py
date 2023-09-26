b7ce6be820114b2f2c0f958d9077bc011ae9b868
Eduardo Islas <126023020+GojiLalo@users.noreply.github.com>
2023-09-25 19:05:24 -0600

    Update lexer.py
    Ahora se guardan los tokens linea por linea y se empezo a escribir la funcion para imprimir los tokens

--- lexer.py
+++ lexer.py
@@ -1,7 +1,21 @@
 import re
 
-operadores = ['+', '-', '*', '/', '%', '>', '<', '!', '&', '|', '^', '~', '(', ')', '{', '}', '[', ']', ';', ',', '.', ':', '#']
-reservadas = ["int","float", "char", "var", "include", "main"]
+operadores = ['+', '-', '*', '/', '%', '>', '<', '!', '&', '|', '^', '~', '(', ')', '{', '}', '[', ']', ';', ',', '.', ':', '#', '"']
+reservadas = ["alignas", "alignof", "and", "and_eq", "asm", "auto", "bitand",
+    "bitor", "bool", "break", "case", "catch", "char", "char8_t",
+    "char16_t", "char32_t", "class", "compl", "concept", "const",
+    "consteval", "constexpr", "const_cast", "continue", "co_await",
+    "co_return", "co_yield", "decltype", "default", "delete", "do",
+    "double", "dynamic_cast", "else", "enum", "explicit", "export",
+    "extern", "false", "float", "for", "friend", "goto", "if",
+    "inline", "int", "long", "mutable", "namespace", "new", "noexcept",
+    "not", "not_eq", "nullptr", "operator", "or", "or_eq", "private",
+    "protected", "public", "register", "reinterpret_cast", "requires",
+    "return", "short", "signed", "sizeof", "static", "static_assert",
+    "static_cast", "struct", "switch", "template", "this", "thread_local",
+    "throw", "true", "try", "typedef", "typeid", "typename", "union",
+    "unsigned", "using", "virtual", "void", "volatile", "wchar_t", "while",
+    "xor", "xor_eq", "cin", "cout", "include", "iostream"]
 
 
 expRegS = [
@@ -13,47 +27,56 @@ expRegS = [
     (r'(\*)', 'MULTIPLICACION'),
     (r'(/)', 'DIVISION'),
     (r'(%)', 'MODULO'),
+    (r'(>)', 'MAYORQUE'),
+    (r'(<)', 'MENORQUE'),
     (r'(\()', 'IZQPAREN'),
     (r'(\))', 'DERPAREN'),
     (r'(;)', 'PUNTOCOMA'),
     (r'({)', 'IZQLLAVE'),
-    (r'(})', 'DERLLAVE')
+    (r'(})', 'DERLLAVE'),
+    (r'(")', 'COMILLAS')
 ]
 
-def obtenerCodigo():
-    archivo = open('codigo.txt', 'r')
-    codigo = archivo.read()
-    return codigo
 
-def obtenerTokens(codigo):
-    tmpTokens = []
-    tokens = []
-    #Separar el codigo en palabras y simbolos
-    for o in operadores:
-        codigo = codigo.replace(o, ' ' + o + ' ')
-    palabras = codigo.split()
-
+def obtenerTokens(linea):
+    listaTokenstmp = []
+    listaTokens = []
+    tmpTokens = linea.split()
     #Clasificar tokens
-    for token in palabras:
+    for t in tmpTokens:
         for expresion, tipoToken in expRegS:
-            if re.match(expresion, token):
-                tmpTokens.append((token, tipoToken))
+            if re.match(expresion, t):
+                listaTokenstmp.append((t, tipoToken))
 
     #Encontrar reservadas
-    for token, tipoToken in tmpTokens:
+    for token, tipoToken in listaTokenstmp:
         if token in reservadas:
-            tokens.append((token, "PR"))
+            listaTokens.append((token, "PR"))
         if token not in reservadas:
-            tokens.append((token, tipoToken))
-
-    for token in tokens:
-        print(token)
+            listaTokens.append((token, tipoToken))
+    return listaTokens
 
 
+def obtenerCodigo():
+    lineas = []
+    listaTokens = []
+    with open('codigo.txt') as archivo:
+        for linea in archivo:
+            for o in operadores:
+                linea = linea.replace(o, ' ' + o + ' ')
+            listaTokens.append(obtenerTokens(linea))
+    return listaTokens
+
+
+def imprimirTokens(listaTokens):
+    print("["+' '.join(listaTokens[0][0]) + "]")
 
 
 
 
 
-codigo = obtenerCodigo()
-obtenerTokens(codigo)
+
+
+
+
+imprimirTokens(obtenerCodigo())
